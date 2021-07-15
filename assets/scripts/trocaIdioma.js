085553d48f749e0d4a2e4b6f6e5d11093c3bc5ed
ArthurMaciel95 <arthurnmrocha@gmail.com>
2021-07-15 12:52:54 -0300

    changes about language, but not implemented

--- assets/scripts/trocaIdioma.js
+++ assets/scripts/trocaIdioma.js
@@ -1,12 +1,57 @@
-async function language() {
-  let option = {
-    method: 'GET',
-  };
+// async function getDataLanguage() {
+//   const response = await fetch('http://localhost:5500/assets/language/en.json');
+//   const data = await response.json();
+//   return data;
+// }
 
-  const response = await fetch('../language/en.json', option);
-  const data = await response.json();
+// document.querySelector('#en-img').addEventListener('click', changeLanguage());
 
-  return console.log(data);
-}
+// document.querySelector('#es-img').addEventListener('click', () => {});
 
-language();
+// function changeLanguage() {
+//   let data = getDataLanguage();
+//   let {
+//     navbar: { sobre, Unidades, Passagens, Contato, Pacotes },
+//     hero: { h1, h2, button1, button2 },
+//     banner: { banner_h2, banner_h3 },
+//     destinos: {
+//       h2_destino,
+//       p_destino,
+//       espanha,
+//       estados_unidos,
+//       franca,
+//       japao,
+//       portugal,
+//       italia,
+//     },
+//   } = data;
+//   console.log(data);
+//   const menu = document.querySelectorAll('[data-menu]');
+//   menu[0].innerHTML = sobre;
+//   menu[1].innerHTML = Passagens;
+//   menu[2].innerHTML = Unidades;
+//   menu[3].innerHTML = Pacotes;
+//   menu[4].innerHTML = Contato;
+
+//   const hero = document.querySelectorAll('[data-hero]');
+
+//   hero[0].innerHTML = h1;
+//   hero[1].innerHTML = h2;
+//   hero[2].innerHTML = button1;
+//   hero[3].innerHTML = button2;
+
+//   const banner = document.querySelectorAll('[data-banner]');
+//   banner[0].innerHTML = banner_h2;
+//   banner[1].innerHTML = banner_h3;
+
+//   const destino = document.querySelectorAll('[data-destinos]');
+
+//   destino[0].innerHTML = h2_destino;
+//   destino[1].innerHTML = p_destino;
+//   destino[2].innerHTML = estados_unidos;
+//   destino[3].innerHTML = franca;
+//   destino[4].innerHTML = japao;
+//   destino[5].innerHTML = portugal;
+//   destino[6].innerHTML = italia;
+//   destino[7].innerHTML = espanha;
+// }
